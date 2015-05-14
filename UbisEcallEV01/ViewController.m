98cb19be8a51b3ca9a4012105c19f92ae865b9e6
Chang soonHwang <changsoonhwang@Changs-Mac.local>
2015-05-14 20:13:21 +0900

    d

--- UbisEcallEV01/ViewController.m
+++ UbisEcallEV01/ViewController.m
@@ -2,6 +2,9 @@
 #import "ViewController.h"
 #import "CAllServer.h"
 #import "IdentViewController.h"
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <netinet/in.h>
+
 @interface ViewController ()
 
 @end
@@ -17,6 +20,18 @@ NSString* idForVendor;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //network check
+    
+    NSLog(@"Connection ststus : %@", [self connectedToNetwork] ? @"YES" : @"NO");
+    
+    //나중에 적용하자..Message Box
+    //---------------------------------------------------------
+
+    
+    
+    
+    
     _locationTxt.delegate = self;
     UIDevice *device = [UIDevice currentDevice];
     idForVendor = [device.identifierForVendor UUIDString];
@@ -152,6 +167,50 @@ NSString* idForVendor;
     }
     
     return YES;
+}
+
+
+- (BOOL) connectedToNetwork {
+    // 0.0.0.0 주소를 만든다.
+    struct sockaddr_in zeroAddress;
+    bzero(&zeroAddress, sizeof(zeroAddress));
+    zeroAddress.sin_len = sizeof(zeroAddress);
+    zeroAddress.sin_family = AF_INET;
+    
+    // Reachability 플래그를 설정한다.
+    SCNetworkReachabilityRef defaultRouteReachability = SCNetworkReachabilityCreateWithAddress(NULL, (struct sockaddr *)&zeroAddress);
+    SCNetworkReachabilityFlags flags;
+    
+    BOOL didRetrieveFlags = SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags);
+    CFRelease(defaultRouteReachability);
+    
+    if (!didRetrieveFlags)
+    {
+        
+              NSLog(@" Error. Could not recover network reachability flags");
+        return 0;
+    }
+    
+    // 플래그를 이용하여 각각의 네트워크 커넥션의 상태를 체크한다.
+    BOOL isReachable = flags & kSCNetworkFlagsReachable;
+    BOOL needsConnection = flags & kSCNetworkFlagsConnectionRequired;
+    BOOL nonWiFi = flags & kSCNetworkReachabilityFlagsTransientConnection;
+    
+    return ((isReachable && !needsConnection) || nonWiFi) ? YES : NO;
+              
+              
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"Clicked");
+    // OK 버튼을 눌렀을 때 버튼Index가 1로 들어감
+    
+    if (buttonIndex == 1) {
+        NSLog(@"Clicked YES");
+        exit(0);
+        
+    }
 }
 
 @end
